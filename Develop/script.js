c4b7179e2ab3ef43429ebe32b4263856ae91ce0c
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-09-27 16:20:35 -0700

    added initial button files, added arrays for each character, added randomg index generator

---
+++ Develop/script.js
@@ -0,0 +1,60 @@
+// Assignment Code
+
+// Global Variables
+var generateBtn = document.querySelector("#generate");
+
+// Character Arrays
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var specialChar =[' ','!','"','#','$','%','&',"'",'(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','\'',']','^','_','`','{','|','}','~'];
+
+function RNG (num) {
+  var index = Math.floor(Math.random() * num.length)
+  var randomItem = num[index]
+  console.log('Random Character: ', randomItem)
+  return randomItem;
+}
+RNG(numbers);
+RNG(lowerCase);
+RNG(upperCase);
+RNG(specialChar)
+
+// method .concat() to combine arrays into one
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
